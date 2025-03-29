5fb964e5ad151ffcaaae49242389149ed5de7c2b
FabiaonGLentini <fabiano.g.lentini@gmail.com>
2025-03-29 15:57:21 -0700

    Delete: readme instructions

--- BSC HACK V02/src/components/AddButton.tsx
+++ BSC HACK V02/src/components/AddButton.tsx
@@ -11,7 +11,7 @@ const AddButton: React.FC = () => {
   return (
     <button
       onClick={handleAddClick}
-      className="fixed bottom-6 left-1/2 transform -translate-x-1/2 z-20 bg-shortsizzle-red hover:bg-red-600 transition-colors w-14 h-14 rounded-full flex items-center justify-center shadow-lg"
+      className="fixed bottom-6 left-1/2 transform -translate-x-1/2 z-20 bg-shortsizzle-red hover:bg-blue-600 transition-colors w-14 h-14 rounded-full flex items-center justify-center shadow-lg"
       aria-label="Add new content"
     >
       <Plus className="w-8 h-8 text-white" />

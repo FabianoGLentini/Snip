62f98ba58bea231f31f5e1900957f48da443e43f
FabiaonGLentini <fabiano.g.lentini@gmail.com>
2025-03-30 00:33:08 -0700

    Add: route react element for laoding in app.jsx file

--- BSC HACK V02/src/App.jsx
+++ BSC HACK V02/src/App.jsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Auth from "./pages/Auth"
 import PdfUploadModal from "./pages/PdfUploadModal"
+import LoadingScreen from "./pages/LoadingScreen"
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/pdf" element={<PdfUploadModal />} />
+          <Route path="/loading" element={<LoadingScreen/>}/>
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
